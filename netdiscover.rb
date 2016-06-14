a8e7689d226e3b6ac628ee0f5692f9d17f37d9cf
Giuseppe Marco Randazzo <gmrandazzo@gmail.com>
2016-06-14 10:28:51 +0200

    Update netdiscover

--- netdiscover.rb
+++ netdiscover.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Netdiscover < Formula
   homepage "http://nixgeneration.com/~jaime/netdiscover/"
-  url "http://nixgeneration.com/~jaime/netdiscover/releases/netdiscover-0.3-beta6.tar.gz"
+  url "http://downloads.sourceforge.net/project/netdiscover/netdiscover/0.3-beta6/netdiscover-0.3-beta6.tar.gz"
   sha1 "b6dfb89a0cbd14e398ae01f358bc56719c994856"
 
   head do
-    url "https://github.com/zeld/netdiscover.git"
+    url "https://github.com/alexxy/netdiscover.git"
     depends_on "cmake" => :build
     depends_on "gawk"
   end
@@ -14,6 +14,7 @@ class Netdiscover < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libnet"
+  depends_on "libpcap"
 
   def install
     if build.head?
