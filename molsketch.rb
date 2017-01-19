4dd5c464ca33449b6c8a509f214b868ae92b3c4b
Giuseppe Marco Randazzo <gmrandazzo@gmail.com>
2017-01-19 11:17:41 +0100

    Update molsketch

--- molsketch.rb
+++ molsketch.rb
@@ -2,24 +2,17 @@ require "formula"
 
 class Molsketch < Formula
   homepage "http://molsketch.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/molsketch/Molsketch/Carbon%200.2.0/Molsketch-0.2.0-Source.tar.gz"
-  sha256 "05e058bf71fc99e5dda56ef1779a82c8885b2001d1af5dce92d959bf56d8a5d0"
+  url "https://downloads.sourceforge.net/project/molsketch/Molsketch/Beryllium%200.4.0/Molsketch-0.4.0-src.tar.gz"
+  sha256 "5ff8d8fe9c8824bdd9b84edc4db1254cc729ab72c7e2c745fbea392145eb3900"
 
   depends_on "cmake" => :build
   depends_on "open-babel"
-  depends_on "qt"
-  depends_on "qt-assistant-compat"
-
-  patch do
-    url "https://raw.githubusercontent.com/zeld/patch/master/Molsketch_0.2.0_BuildPatch.patch"
-    sha256 "af70c93f39920dfcaf1a894865d77f5860d2fcd0c7f50f1d220715f750dc861e"
-  end
+  depends_on "qt5"
 
   def install
     mkdir "build" do
       args = std_cmake_args
-      args << "-DQT_ASSISTANT_LIBRARY=#{Formula["qt-assistant-compat"].opt_lib}/QtAssistant.framework/QtAssistant"
-      args << "-DQT_ASSISTANT_INCLUDE=#{Formula["qt-assistant-compat"].opt_include}/QtAssistant/"
+      args << "-DCMAKE_PREFIX_PATH=#{Formula["qt5"]}"
       system "cmake", "..", *args
       system "make"
       system "make install"
