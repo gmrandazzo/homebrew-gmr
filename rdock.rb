0b78aba017de82a4b8e8396292105a9f0b0f2445
marco <marco@chemify.io>
2025-11-04 11:39:41 +0100

    Fix rdock install problem

--- rdock.rb
+++ rdock.rb
@@ -1,7 +1,6 @@
 class Rdock < Formula
   desc "Molecular docking program for virtual screening"
   homepage "https://rdock.github.io/"
-  url "https://rdock.github.io/"
   version "latest"
   license "GPL-3.0-or-later"
   head "https://github.com/CBDD/rDock.git", branch: "main"
@@ -10,10 +9,8 @@ class Rdock < Formula
   depends_on "popt"
 
   def install
-    # Set the correct C++ compiler from Homebrew’s GCC installation
     gcc_ver = Formula["gcc"].version.major
     ENV["CXX"] = "#{HOMEBREW_PREFIX}/opt/gcc/bin/g++-#{gcc_ver}"
-
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
   end
